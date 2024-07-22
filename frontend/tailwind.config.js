4589a15f57ec84a028450fa3e218d3014bf1dc5a
Abhijeet Singh <abhijeetsingh13b@gmail.com>
2024-07-22 19:52:08 +0530

    modify frontend setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#FF385C',
+        accent: '#E00B41'
+      }
+    },
   },
   plugins: [],
 }
